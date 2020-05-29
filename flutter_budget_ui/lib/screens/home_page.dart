f255d8361b157addd0f4f6a56ff29a1a14e48ca8
Abdi Mussa <abdimussa87@gmail.com>
2020-05-29 18:39:25 +0300

    Finished working on the bar charts

--- flutter_budget_ui/lib/screens/home_page.dart
+++ flutter_budget_ui/lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_ui/data/data.dart';
+import 'package:flutter_budget_ui/models/category_model.dart';
 import 'package:flutter_budget_ui/widgets/bar_chart.dart';
 
 class HomePage extends StatefulWidget {
@@ -40,21 +41,120 @@ class _HomePageState extends State<HomePage> {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return Container(
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(color: Colors.black12, offset: Offset(0, 2))
-                    ],
-                  ),
-                  child: BarChart(weeklySpending),
-                );
+                if (index == 0) {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(color: Colors.black12, offset: Offset(0, 2))
+                      ],
+                    ),
+                    child: BarChart(weeklySpending),
+                  );
+                } else {
+                  Category category = categories[index - 1];
+                  return _buildCategoryView(category);
+                }
               },
-              childCount: 1,
+              childCount: 1 + categories.length,
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  _buildCategoryView(Category category) {
+    double totalUsedAmount = 0;
+    category.expenses.forEach((expense) {
+      totalUsedAmount += expense.cost;
+    });
+    
+    Color barColor = Colors.grey;
+    //checking percentage values for barColor
+    if (category.maxAmount - totalUsedAmount < category.maxAmount * 30 / 100) {
+      barColor = Colors.red;
+    } else if (category.maxAmount - totalUsedAmount <
+        category.maxAmount * 60 / 100) {
+      barColor = Colors.yellow;
+    } else if (category.maxAmount - totalUsedAmount <
+        category.maxAmount * 90 / 100) {
+      barColor = Colors.green;
+    }
+
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+      padding: EdgeInsets.all(20),
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 2),
+            blurRadius: 6,
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                category.name,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Text(
+                '\$${(category.maxAmount - totalUsedAmount).toStringAsFixed(2)}/ \$${category.maxAmount.toStringAsFixed(2)}',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              double percent  = (category.maxAmount- totalUsedAmount) / category.maxAmount;
+              double barWidth = percent * constraints.maxWidth;
+              print("Percent = $percent bar width = $barWidth");
+              //useful checking to see if totalUsedAmount doesn't outweight maxAmount
+              if(barWidth<0){
+                barWidth = 0;
+              }
+              
+              return Stack(
+                children: <Widget>[
+                  Container(
+                    height: 18,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  Container(
+                    width: barWidth,
+                    height: 18,
+                    decoration: BoxDecoration(
+                      color: barColor,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
--- flutter_budget_ui/lib/widgets/bar_chart.dart
+++ flutter_budget_ui/lib/widgets/bar_chart.dart
@@ -6,44 +6,89 @@ class BarChart extends StatelessWidget {
   BarChart(this.weeklySpending);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          'Weekly Spending',
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 1.2),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              iconSize: 30,
-              onPressed: () {},
-            ),
-            Text(
-              'May 29, 2020 - June 04, 2020',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+    double _mostExpensive = 0;
+    weeklySpending.forEach((amount) {
+      if (amount > _mostExpensive) {
+        _mostExpensive = amount;
+      }
+    });
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Weekly Spending',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, letterSpacing: 1.2),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                iconSize: 30,
+                onPressed: () {},
               ),
-            ),
-            IconButton(
-              icon: Icon(Icons.arrow_forward),
-              iconSize: 30,
-              onPressed: () {},
-            ),
-          ],
-        ),
-        Row(
-          children: <Widget>[
-            Bar(
+              Text(
+                'May 29, 2020 - June 04, 2020',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                iconSize: 30,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Bar(
                 label: 'Su',
                 amountSpent: weeklySpending[0],
-                mostExpensive: 200),
-          ],
-        ),
-      ],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'Mo',
+                amountSpent: weeklySpending[1],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'Tu',
+                amountSpent: weeklySpending[2],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'We',
+                amountSpent: weeklySpending[3],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'Th',
+                amountSpent: weeklySpending[4],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'Fr',
+                amountSpent: weeklySpending[5],
+                mostExpensive: _mostExpensive,
+              ),
+              Bar(
+                label: 'Sa',
+                amountSpent: weeklySpending[6],
+                mostExpensive: _mostExpensive,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -52,17 +97,51 @@ class Bar extends StatelessWidget {
   final label;
   final amountSpent;
   final mostExpensive;
+  final double _maxBarHeight = 150;
   Bar({
     this.label,
     this.amountSpent,
     this.mostExpensive,
   });
+
   @override
   Widget build(BuildContext context) {
+    final barHeight = amountSpent / mostExpensive * _maxBarHeight;
     return Column(
       children: <Widget>[
-        Text('\$${amountSpent.toStringAsFixed(2)}'),
-        Text(label)
+        Text(
+          '\$${amountSpent.toStringAsFixed(2)}',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: 6,
+        ),
+        Container(
+          height: barHeight,
+          width: 20,
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(6),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  offset: Offset(0, 2),
+                  blurRadius: 6,
+                )
+              ]),
+        ),
+        SizedBox(
+          height: 6,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        )
       ],
     );
   }
